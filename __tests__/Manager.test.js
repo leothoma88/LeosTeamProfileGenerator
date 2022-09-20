98b0aa74f4b77f395285f9b753a9facff2431d18
Darrell Thomas <darrellthomas@Darrells-MacBook-Pro.local>
2022-09-20 17:12:51 -0400

    Manager test added

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require("../lib/Manager");
+
+test("OfficeNumber", () => {
+  const testValue = 1234;
+  const e = new Manager("Darrell Thomas", 1, "leothoma88@gmail.com", testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
+
+
+test("getOffice()", () => {
+  const testValue = 1234;
+  const e = new Manager("Darrell Thomas", 1, "leothoma88@gmail.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+})
+
+test("getRole()", () => {
+    const testValue = "Manager";
+    const e = new Manager("Darrell Thomas", 1, "leothoma88@gmail.com", 1234);
+    expect(e.getRole()).toBe(testValue);
+  });
+  
